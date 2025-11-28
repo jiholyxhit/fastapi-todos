8948003be76ecba3c6392f12abe51072d05f57eb
jihowoo <jihobbit8677@gmail.om>
2025-11-27 16:59:37 -0800

    refactor: api router refactoring

--- todos/src/main.py
+++ todos/src/main.py
@@ -1,102 +1,13 @@
-from typing import List
+from fastapi import FastAPI
 
-from fastapi import FastAPI, Body, HTTPException, Depends
-from sqlalchemy.orm import Session
-
-from database.connection import get_db
-from database.orm import ToDo
-from database.repository import get_todos, get_todo_by_todo_id, create_todo, update_todo, delete_todo
-from schema.request import CreateToDoRequest
-
-from schema.response import ToDoListSchema, ToDoSchema
+from api import todo
 
 app = FastAPI()
+app.include_router(todo.router)
 
 
 @app.get("/")
 def health_check_handler():
     return {"ping": "pong"}
 
-# todo_data = {
-#     1: {q
-#         "id": 1, "contents": "laundry", "is_done": True
-#     },
-#     2: {
-#         "id": 2, "contents": "meal prep", "is_done": False
-#     },
-#     3: {
-#         "id": 3, "contents": "pre-class work", "is_done": False
-#     },
-# }
-
-
-@app.get("/todos", status_code=200)
-def get_todos_handler(
-        order: str | None = None,
-        session: Session = Depends(get_db),
-):
-    todos: List[ToDo] = get_todos(session=session)
-    # res = list(todo_data.values())
-    if order and order == "DESC":
-        # return todos[::-1]
-        return ToDoListSchema(
-            todos = [ToDoSchema.model_validate(todo) for todo in todos[::-1]]
-        )
-    # return todos
-    return ToDoListSchema(
-        todos = [ToDoSchema.model_validate(todo) for todo in todos]
-    )
-
-
-@app.get("/todos/{todo_id}", status_code=200)
-def get_todo_handler(todo_id: int, session: Session  = Depends(get_db)):
-    # todo = todo_data.get(todo_id)
-    todo: ToDo | None = get_todo_by_todo_id(session, todo_id)
-    if todo:
-        return ToDoSchema.model_validate(todo)
-    raise HTTPException(status_code=404, detail="Todo Not Found")
-
-
-@app.post("/todos", status_code=201)
-def create_todo_handler(
-        request: CreateToDoRequest,
-        session: Session  = Depends(get_db),
-):
-    todo: ToDo = ToDo.create(request=request) #make an ORM model obj made with req schema
-    todo: ToDo = create_todo(session = session, todo = todo) #pass the ORM model obj to DB through repository function and then refresh to return back
-    return ToDoSchema.model_validate(todo)
-    # return todo_data[request.id]
-
-
-@app.patch("/todos/{todo_id}",status_code=200)
-def update_todo_handler(
-        todo_id: int,
-        is_done: bool = Body(..., embed=True),
-        session: Session = Depends(get_db),
-):
-    todo: ToDo | None = get_todo_by_todo_id(session = session, todo_id = todo_id)
-    if todo:
-        todo.done() if is_done else todo.undone() #Python ternary expression
-        todo: ToDo = update_todo(session = session, todo = todo)
-        return ToDoSchema.model_validate(todo)
-    # todo = todo_data.get(todo_id)
-    # if todo:
-        # todo["is_done"] = is_done
-        # return todo
-    raise HTTPException(status_code=404, detail="Todo Not Found")
-
-
-@app.delete("/todos/{todo_id}", status_code=204)
-def delete_todo_handler(
-        todo_id: int,
-        session: Session = Depends(get_db),
-):
-    todo: ToDo | None = get_todo_by_todo_id(session = session, todo_id = todo_id)
-    if not todo:
-        raise HTTPException(status_code = 404, detail = "ToDo Not Found")
-    delete_todo(session = session, todo_id = todo_id)
-    # todo = todo_data.pop(todo_id, None)
-    # if todo:
-    #     return
-    #raise HTTPException(status_code=404, detail="Todo Not Found")
 
--- todos/src/tests/test_main.py
+++ todos/src/tests/test_main.py
@@ -8,7 +8,7 @@ def test_health_check(client):
 
 def test_get_todos(client, mocker):
     #order ASC
-    mocker.patch("main.get_todos", return_value=[
+    mocker.patch("api.todo.get_todos", return_value=[
         ToDo(id=1, contents="FastAPI Section 0", is_done=True),
         ToDo(id=2, contents="FastAPI Section 1", is_done=False),
     ])
@@ -34,14 +34,14 @@ def test_get_todos(client, mocker):
 
 def test_get_todo(client, mocker):
     #200
-    mocker.patch("main.get_todo_by_todo_id", return_value = ToDo(id=1, contents="todo", is_done=True))
+    mocker.patch("api.todo.get_todo_by_todo_id", return_value = ToDo(id=1, contents="todo", is_done=True))
 
     response = client.get("/todos/1")
     assert response.status_code == 200
     assert response.json() == {"id": 1, "contents": "todo", "is_done": True}
 
     #404
-    mocker.patch("main.get_todo_by_todo_id", return_value = None)
+    mocker.patch("api.todo.get_todo_by_todo_id", return_value = None)
     response = client.get("/todos/1")
     assert response.status_code == 404
     assert response.json() == {"detail": "Todo Not Found"}
@@ -50,7 +50,7 @@ def test_get_todo(client, mocker):
 def test_create_todo(client, mocker):
     create_spy = mocker.spy(ToDo, "create")
     mocker.patch(
-        "main.create_todo",
+        "api.todo.create_todo",
         return_value = ToDo(id=1, contents="todo", is_done=True)
     )
 
@@ -73,14 +73,14 @@ def test_create_todo(client, mocker):
 def test_update_todo(client, mocker):
     #200
     mocker.patch(
-        "main.get_todo_by_todo_id",
+        "api.todo.get_todo_by_todo_id",
         return_value=ToDo(id=1, contents="todo", is_done=True)
     )
 
 
     undone = mocker.patch.object(ToDo, "undone")
     mocker.patch(
-        "main.update_todo",
+        "api.todo.update_todo",
         return_value=ToDo(id=1, contents="todo", is_done=False)
     )
 
@@ -92,7 +92,7 @@ def test_update_todo(client, mocker):
     assert response.json() == {"id": 1, "contents": "todo", "is_done": False}
 
     # 404
-    mocker.patch("main.get_todo_by_todo_id", return_value=None)
+    mocker.patch("api.todo.get_todo_by_todo_id", return_value=None)
     response = client.get("/todos/1")
     assert response.status_code == 404
     assert response.json() == {"detail": "Todo Not Found"}
@@ -103,11 +103,11 @@ def test_update_todo(client, mocker):
 def test_delete_todo(client, mocker):
     #204
     mocker.patch(
-        "main.get_todo_by_todo_id",
+        "api.todo.get_todo_by_todo_id",
         return_value = ToDo(id=1, contents="todo", is_done=True),
     )
     mocker.patch(
-        "main.delete_todo",
+        "api.todo.delete_todo",
         return_value=None,
     )
 
@@ -116,7 +116,7 @@ def test_delete_todo(client, mocker):
 
     #404
     mocker.patch(
-        "main.get_todo_by_todo_id",
+        "api.todo.get_todo_by_todo_id",
         return_value=None,
     )
     response = client.delete("todos/1")
