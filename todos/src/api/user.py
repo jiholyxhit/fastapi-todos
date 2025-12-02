d78b9d6f7c9342e1c2107f8f366c7cbfd7e11220
jihowoo <jihobbit8677@gmail.om>
2025-12-02 15:55:50 -0800

    feature: user api test

--- todos/src/api/user.py
+++ todos/src/api/user.py
@@ -4,6 +4,6 @@ from fastapi import APIRouter
 router = APIRouter(prefix = "/users")
 
 
-@router.post("/sign-up")
+@router.post("/sign-up", status_code=201)
 def user_sign_up_handler():
     return True
